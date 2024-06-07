5bcfaa6b8a0c9a08cf0ff86fef2b2c1d89937d64
Lengalei <ruthlengalei@gmail.com>
2024-06-07 16:15:58 +0300

    about us

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const About = () => {
   return (
     <div className='bord-b border-neutral-900 pb-4'>
-        <h1>About Me</h1>
+        <h1 className='my-20 text-center text-4xl'>About <span className='text-neutral-500'>Me</span></h1>
+        <div className="flex flex-wrap"></div>
       
     </div>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
                     <h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Ruth Lengalei</h1>
                     <span className="bg-gradient-to-r from-pink-300 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Full Stack Developer</span>
 
-                    <p className='my-2 max-w-xl py-6 font-light tracking-tighter '>{HERO_CONTENT}</p>
+                    <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>{HERO_CONTENT}</p>
                 </div>
             </div>
             <div className="w-full lg:w-1/2 lg:p-8">
