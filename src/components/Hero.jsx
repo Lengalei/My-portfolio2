f0194cdb71b3725a32ac500d87479bfb5fad2abb
Lengalei <ruthlengalei@gmail.com>
2024-06-07 12:04:19 +0300

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import {HERO_CONTENT} from "../constants"
+// import profilePic from "../assets/kevinRushProfile"
+
 
 const Hero = () => {
   return (
@@ -6,7 +9,10 @@ const Hero = () => {
         <div className="flex flex-wrap">
             <div className="w-full lg:w-1/2">
                 <div className="flex flex-col items-center lg:items-start">
-                    <h1>Ruth Lengalei</h1>
+                    <h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Ruth Lengalei</h1>
+                    <span className="bg-gradient-to-r from-pink-300 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Full Stack Developer</span>
+
+                    <p className='my-2 max-w-xl py-6 font-light tracking-tighter '>{HERO_CONTENT}</p>
                 </div>
             </div>
         </div>
